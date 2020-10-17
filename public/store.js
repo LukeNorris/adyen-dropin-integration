a22a838b8d330925188543c972a07ac5c6ee2745
Luke Norris <luke.mjn@gmail.com>
2020-10-17 23:33:04 +0200

    added payment modal

--- public/store.js
+++ public/store.js
@@ -26,14 +26,162 @@ function ready() {
     document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
 
-function purchaseClicked() {
-    alert('Thank you for your purchase')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
+// function showFinalResult (response) {
+//     alert('Thank you. Your payment was successfully proccessed.')
+//     // var cartItems = document.getElementsByClassName('cart-items')[0]
+//     // while (cartItems.hasChildNodes()) {
+//     //     cartItems.removeChild(cartItems.firstChild)
+//     // }
+//     // updateCartTotal()
+// }
+
+
+function makePayment (data){
+    var items = []
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    for( let i = 0; i < cartRows.length; i++ ) {
+        const cartRow = cartRows[i]
+        const quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        const quantity = quantityElement.value
+        var id = cartRow.dataset.itemId
+        items.push({
+            id:id,
+            quantity: quantity
+        })
     }
-    updateCartTotal()
+
+    fetch('/makepayment', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'appllcation/json'
+        },
+        body: JSON.stringify({
+            data: data,
+            items: items
+        })
+    }).then(res => {
+        return res.json()
+    }).then(info => {
+        console.log('before',info)
+        return info
+    }).catch(function(error) {
+        console.error(error)
+    })
+ console.log('after',info) 
 }
+   
+
+
+function adyenHandler (){
+    var items = []
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    for( let i = 0; i < cartRows.length; i++ ) {
+        const cartRow = cartRows[i]
+        const quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        const quantity = quantityElement.value
+        var id = cartRow.dataset.itemId
+        items.push({
+            id:id,
+            quantity: quantity
+        })
+    }
+
+    fetch('/purchase', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'appllcation/json'
+        },
+        body: JSON.stringify({
+            items: items
+        })
+    }).then(res => {
+        return res.json()
+    }).then(data => {
+        const configuration = {
+            paymentMethodsResponse: data, // The `/paymentMethods` response from the server.
+            clientKey: adyenClientKey, // Web Drop-in versions before 3.10.1 use originKey instead of clientKey.
+            locale: "en-US",
+            environment: "test",
+            onSubmit: (state, dropin) => {
+                // Your function calling your server to make the `/payments` request
+                makePayment(state.data)
+                  .then(response => {
+                      console.log(response)
+                    if (response.action) {
+                      // Drop-in handles the action object from the /payments response
+                      dropin.handleAction(response.action);
+                    } else {
+                      // Your function to show the final result to the shopper
+                      alert(`Your payment has been successfully proccessed.`)
+                    //   showFinalResult(response);
+                    }
+                  })
+                  .catch(error => {
+                    throw Error(error);
+                  });
+              },
+              onAdditionalDetails: (state, dropin) => {
+                // Your function calling your server to make a `/payments/details` request
+                makeDetailsCall(state.data)
+                  .then(response => {
+                    if (response.action) {
+                      // Drop-in handles the action object from the /payments response
+                      dropin.handleAction(response.action);
+                    } else {
+                      // Your function to show the final result to the shopper
+                      
+                      showFinalResult(response);
+                    }
+                  })
+                  .catch(error => {
+                    throw Error(error);
+                  });
+              },
+              paymentMethodsConfiguration: {
+                card: { // Example optional configuration for Cards
+                  hasHolderName: true,
+                  holderNameRequired: true,
+                  enableStoreDetails: true,
+                  hideCVC: false, // Change this to true to hide the CVC field for stored cards
+                  name: 'Credit or debit card'
+                }
+              }
+        
+           };
+        
+        const checkout = new AdyenCheckout(configuration);
+        const dropin = checkout.create('dropin').mount('#dropin-container');
+
+    }).catch(function(error) {
+        console.error(error)
+    })
+}
+
+
+
+
+
+function purchaseClicked() {
+    // var priceElement = document.getElementsByClassName('cart-total-price')[0]
+    // var price = parseFloat(priceElement.innerText.replace('$', '')) * 100
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    modal.style.display = "block";
+    adyenHandler()
+}
+
+
+
+// var cartItems = document.getElementsByClassName('cart-items')[0]
+// while (cartItems.hasChildNodes()) {
+//     cartItems.removeChild(cartItems.firstChild)
+// }
+// updateCartTotal()
 
 function removeCartItem(event) {
     var buttonClicked = event.target
@@ -55,13 +203,15 @@ function addToCartClicked(event) {
     var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
+    var id = shopItem.dataset.itemId
+    addItemToCart(title, price, imageSrc, id)
     updateCartTotal()
 }
 
-function addItemToCart(title, price, imageSrc) {
+function addItemToCart(title, price, imageSrc, id) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
+    cartRow.dataset.itemId = id
     var cartItems = document.getElementsByClassName('cart-items')[0]
     var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
     for (var i = 0; i < cartItemNames.length; i++) {
@@ -101,3 +251,20 @@ function updateCartTotal() {
     total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target != modal) {
+      modal.style.display = "none";
+    }
+  }
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
+import fs from 'fs'
+import axios from 'axios'
 
 dotenv.config()
+
+
+const adyenApiKey = process.env.ADYEN_API_KEY
+const adyenClientKey = process.env.ADYEN_CLIENT_KEY
+const merchantName = process.env.MERCHANT_ACCOUNT_NAME
+const paymentMethodsURL = 'https://checkout-test.adyen.com/v64/paymentMethods'
+const paymentsURL = 'https://checkout-test.adyen.com/v64/payments'
+
 
 const app = express()
 
@@ -10,6 +20,107 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+app.use(express.json())
+
+
+app.get('/store', (req, res) => {
+    fs.readFile('items.json', function(error, data){
+        if(error) {
+            res.status(500).end()
+        } else {
+            res.render('store.ejs', {
+                adyenClientKey: adyenClientKey,
+                items: JSON.parse(data)
+            })
+        }
+    })
+})
+
+
+app.post('/purchase', (req, res) => {
+    fs.readFile('items.json', (error, data) => {
+        if(error) {
+            res.status(500).end()
+        } else {
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+
+            req.body.items.forEach(item => {
+                const itemJson = itemsArray.find(i => {
+                  return i.id == item.id
+                })
+                return total = total + itemJson.price * item.quantity
+            })
+
+            const customerInfo = {
+                merchantAccount: merchantName,
+                amount:[total],
+                channel: "Web",
+                countryCode: "NL"
+            }  
+            const paymentMethodsHeaders = {
+                "Content-Type": "application/json",
+                "X-API-Key": adyenApiKey
+            }
+
+            axios.post( paymentMethodsURL, customerInfo, {
+                headers: paymentMethodsHeaders
+            }).then(response => {
+                res.json(response.data)
+            }).catch(error => {
+                console.log(error, 'charge fail');
+            });
+        }
+    })
+})
+
+
+app.post('/makePayment', (req, res) => {
+    fs.readFile('items.json', (error, data) => {
+        if(error) {
+            res.status(500).end()
+        } else {
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+            req.body.items.forEach(item => {
+                const itemJson = itemsArray.find(i => {
+                  return i.id == item.id
+                })
+                return total = total + itemJson.price * item.quantity
+            })
+
+            const paymentMethod = req.body.data.paymentMethod
+            const paymentRequestParams = {
+                merchantAccount: merchantName,
+                amount:{
+                    "currency": "EUR",
+                    "value": total
+                },
+                paymentMethod: paymentMethod,
+                reference:  'luke_checkoutChallenge',
+                returnUrl: 'http://localhost:5000',
+                channel: "Web",
+                countryCode: "NL"
+            }  
+            const paymentHeaders = {
+                "Content-Type": "application/json",
+                "X-API-Key": adyenApiKey
+            }
+
+            axios.post( paymentsURL, paymentRequestParams, {
+                headers: paymentHeaders
+            }).then(response => {
+                res.json(response.data)
+            }).catch(error => {
+                console.log(error, 'charge fail');
+            });
+        }
+    })
+}) 
+
+
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(PORT, console.log(`server running on port ${PORT}`.yellow.bold))
